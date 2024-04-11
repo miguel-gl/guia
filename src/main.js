6b2c27b716a704292a797cacde07e30e1fc32618
Miguel Gonzalez <engineering112@multiplica.com>
2024-04-11 16:34:37 -0600

    Implementacion de extencion

--- src/main.js
+++ src/main.js
@@ -1,22 +1,33 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 
+let mainWindow;
+
 function createWindow() {
-  const win = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    autoHideMenuBar: true,
     webPreferences: {
-        contextIsolation: false,
-        nodeIntegration: true,
-        nodeIntegrationInWorker: true,
-        enableRemoteModule: true
+      contextIsolation: false,
+      nodeIntegration: true,
+      nodeIntegrationInWorker: true,
+      enableRemoteModule: true
     }
   });
 
-  win.loadFile('src/index.html');
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -24,44 +35,43 @@ app.on('window-all-closed', () => {
   }
 });
 
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
-
-autoUpdater.setFeedURL({
-  provider: 'github',
-  owner: 'miguel-gl',
-  repo: 'guia',
-  releaseType: 'release'
-});
-
-// Escuchar eventos de actualización
-autoUpdater.on('checking-for-update', () => {
-  console.log('Buscando actualizaciones...');
-});
-autoUpdater.on('update-available', () => {
-  mainWindow.webContents.send('update-available');
-});
-autoUpdater.on('update-not-available', () => {
-  console.log('No hay actualizaciones disponibles.');
-});
-autoUpdater.on('update-downloaded', () => {
-  console.log('Actualización descargada, se instalará al reiniciar la aplicación.');
-  mainWindow.webContents.send('update-downloaded');
-});
-
-// Cuando se recibe el mensaje de "Descargar"
-ipcMain.on('download-update', () => {
-  autoUpdater.downloadUpdate();
-});
-
-// Cuando se recibe el mensaje de "Instalar"
-ipcMain.on('install-update', () => {
-  autoUpdater.quitAndInstall();
-});
-
+// Comprobar actualizaciones al iniciar la aplicación
 app.on('ready', () => {
+  // Configurar el servidor de actualización
+  const { autoUpdater } = require('electron-updater');
+  
+  autoUpdater.setFeedURL({
+    url: 'https://github.com/miguel-gl/guia/releases',
+    provider: 'github',
+    owner: 'miguel-gl',
+    repo: 'guia',
+    releaseType: 'release'
+  });
+
+  // Manejar eventos de actualización
+  autoUpdater.on('checking-for-update', () => {
+    console.log('Buscando actualizaciones...');
+  });
+  autoUpdater.on('update-available', () => {
+    mainWindow.webContents.send('update-available');
+  });
+  autoUpdater.on('update-not-available', () => {
+    console.log('No hay actualizaciones disponibles.');
+  });
+  autoUpdater.on('update-downloaded', () => {
+    console.log('Actualización descargada, se instalará al reiniciar la aplicación.');
+    mainWindow.webContents.send('update-downloaded');
+  });
+
+  // Escuchar mensajes de actualización desde el proceso de renderizado
+  ipcMain.on('download-update', () => {
+    autoUpdater.downloadUpdate();
+  });
+
+  ipcMain.on('install-update', () => {
+    autoUpdater.quitAndInstall();
+  });
+
+  // Comprobar actualizaciones
   autoUpdater.checkForUpdates();
 });
